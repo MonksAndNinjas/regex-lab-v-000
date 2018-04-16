a93b23318b8d212263b70975af2f2a8642f14862
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-16 05:36:16 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,8 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.each do |number|
-    if number.scan(/[\w]/).length === 10
+    if phone.scan(/[\w]/).length === 10
       true
     end
   end
